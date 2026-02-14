f52d56e6da4e080cdcd05e00878ba8b59a64b470
Humberto MDC <humberto04@gmail.com>
2026-02-13 16:27:50 -0800

    Initial Commit

---
+++ Brackets/BracketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BracketsApp.swift
+//  Brackets
+//
+//  Created by Humberto on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BracketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
